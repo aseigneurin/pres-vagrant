5ab96b9928bb024063617f90962b1186f72f8f3a
Alexis Seigneurin <alexis@seigneurin.com>
2015-09-22 18:27:23 +0200

    - starting from scratch

--- 2_vagrant-with-standard-boxes/scripts/provision-backend-1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-apt-get -y install apache2
-
-echo "Je suis le back-end #1" > /var/www/index.html
--- 2_vagrant-with-standard-boxes/scripts/provision-backend-2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-apt-get -y install apache2
-
-echo "Je suis le back-end #2" > /var/www/index.html
